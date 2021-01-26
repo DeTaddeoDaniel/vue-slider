a400e15e5c28ddd8182249453a6c3da5aae94e46
Daniel2711 <detaddeo.d@gmail.com>
2021-01-26 15:48:38 +0100

    Add pointer to slider html

--- module.js
+++ module.js
@@ -5,39 +5,48 @@ const slider = new Vue ({
         srcImg:'img/slider-img-0.jpg',
         altImg:'Forest under the start',
         TitleImg:'Forest under the start',
+        descrImg:'lorem lipsu 1',
 
         imgs: [
             {
                 scr: 'img/slider-img-0.jpg',
                 text: 'Forest under the start',
+                descr:'lorem lipsu 0',
             },
             {
                 scr: 'img/slider-img-1.jpg',
                 text: 'Lake in the montain',
+                descr:'lorem lipsu 1',
             },
             {
                 scr: 'img/slider-img-2.jpg',
                 text: 'Montain from sky',
+                descr:'lorem lipsu 2',
             },
             {
                 scr: 'img/slider-img-3.jpg',
                 text: 'Forest in one day',
+                descr:'lorem lipsu 3',
             },
             {
                 scr: 'img/slider-img-4.jpg',
                 text: 'Lakes of ice',
+                descr:'lorem lipsu 4',
             },
             {
                 scr: 'img/slider-img-5.jpg',
-                text: 'Landscape montain with river',
+                text: 'Landscap',
+                descr:'lorem lipsu 5',
             },
             {
                 scr: 'img/slider-img-6.jpg',
                 text: 'river in the montain',
+                descr:'lorem lipsu 6',
             },
             {
                 scr: 'img/slider-img-7.jpg',
                 text: 'small lake in the montain',
+                descr:'lorem lipsu 7',
             },
         ],
         
@@ -56,6 +65,7 @@ const slider = new Vue ({
             this.srcImg = dataInfo.scr;
             this.altImg = dataInfo.text;
             this.TitleImg = dataInfo.text;
+            this.descrImg = dataInfo.descr;
             
             console.log(dataInfo.text);
         },
@@ -71,6 +81,7 @@ const slider = new Vue ({
             this.srcImg = dataInfo.scr;
             this.altImg = dataInfo.text;
             this.TitleImg = dataInfo.text;
+            this.descrImg = dataInfo.descr;
 
             console.log(this.counter);
         }
